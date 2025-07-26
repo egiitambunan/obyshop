cccb0cda4dcde9d53b9819df04b8745070c45b43
egiitambunan <144206075+egiitambunan@users.noreply.github.com>
2025-07-26 14:41:14 +0700

    fetch-data.js

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,3 +1,19 @@
+let produkData = [];
+
+async function fetchProduk() {
+  try {
+    const res = await fetch("https://obyshop-backend-production-4831.up.railway.app/api/products");
+    const data = await res.json();
+
+    produkData = data.map(p => ({
+      nama: p.nama || p.name,
+      harga: p.harga || p.price,
+      gambar: https://obyshop-backend-production-4831.up.railway.app/uploads/products/${p.imageFilename},
+    }));
+  } catch (err) {
+    console.error("❌ Gagal mengambil produk:", err);
+  }
+}
 document.addEventListener("DOMContentLoaded", async () => {
   try {
     // Ambil konten umum dari backend
